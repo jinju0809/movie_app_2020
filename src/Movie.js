6409a965f3f1825c58f0730759f240db0b1a5816
jinju0809 <jjyoun48@gmail.com>
2021-07-19 14:37:50 +0900

    4.3 도중까지

--- src/Movie.js
+++ src/Movie.js
@@ -1,20 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 //state가 필요 없으면 class component를 쓸 필요 없음
 //function 쓴다.
-function Movie({id, year, title, summary, poster}){
-    return <h1>{title}</h1>;
+function Movie({ year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+        <img src={poster} alt={title} title={title}/>
+      <div className="movie_data">
+        <h3 className="movie_title" style={{backgroundColor: "red"}}>{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <p className="movie_summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 //id 34043
 //우리가 얻어 올 props를 찾는다.
 Movie.propTypes = {
-    id: PropTypes.number.isRequired,
-    year: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired,
-    
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 export default Movie;
