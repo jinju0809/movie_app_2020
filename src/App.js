42c16c341a25ab66552765e809968482f6f34fc3
jinju0809 <jjyoun48@gmail.com>
2021-07-19 11:10:14 +0900

    componentDidMount를 가지고 일정 시간 뒤에 내용이 바뀌는... 이벤트?만들기

--- src/App.js
+++ src/App.js
@@ -3,43 +3,18 @@ import PropTypes from "prop-types";
 
 //state 사용하기, state는 자바스크립트!
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    console.log("hello constructor");
-  }
-  //변해는 데이터를 위한 state, state는 Object이다.
   state = {
-    count: 0
-  };
-   
-  add = () => {
-    console.log("add");
-    this.setState(current => ({count: current.count +1 }))
-  };
-  minus = () => {
-    console.log("minus");
-    this.setState(current => ({count: current.count -1})) 
-    
-  };
-  //mounting
+    isLoading: true
+  }
   componentDidMount(){
-    console.log("component rendered");
-  }
-  //updating
-  componentDidUpdate(){
-    console.log("I just updated");
-  }
-
-  //unmounting - 확인은 되지 않은... 
-  componentWillUnmount(){
-    console.log("Good bye");
+    setTimeout(()=>{
+      this.setState({isLoading: false});
+    }, 6000);
   }
    render(){
-     console.log("Im rendering");
+     const {isLoading} = this.state;
      return <div>
-       <h1>The number is: {this.state.count}</h1>
-       <button onClick={this.add}>Add</button>
-       <button onClick={this.minus}>Minus</button>
+       {isLoading? "Loading": "We are ready"}
        </div>;
    }
 }
