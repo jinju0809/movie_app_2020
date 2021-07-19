dc1299a9cd32ee037139819245c6e8b69cd25ad1
jinju0809 <jjyoun48@gmail.com>
2021-07-19 09:52:12 +0900

    proptypes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,68 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-
-function Food({name, picture}) {
-  return <div>
-    <h2>I like {name}</h2>
-    <img src={picture} />
-
-  </div>
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h3>Rating {rating}/5.0</h3>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
+Food.propTypes = {
+  //props에 대한 설명
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 //좋아하는 음식의 배열
-const foodILike =[
+const foodILike = [
   {
+    id: 1,
     name: "kimchi",
     image: "https://i.ytimg.com/vi/eTucCw1w6Ak/maxresdefault.jpg",
+    
   },
   {
-    name:"삼겹살",
+    id: 2,
+    name: "삼겹살",
     image: "https://i.ytimg.com/vi/eTucCw1w6Ak/maxresdefault.jpg",
+    rating: 4.3,
   },
   {
-    name:"비빔밥",
+    id: 3,
+    name: "비빔밥",
     image: "https://i.ytimg.com/vi/eTucCw1w6Ak/maxresdefault.jpg",
+    rating: 3.4,
   },
   {
-    name:"주꾸미",
+    id: 4,
+    name: "주꾸미",
     image: "https://i.ytimg.com/vi/eTucCw1w6Ak/maxresdefault.jpg",
+    rating: 4.3,
   },
   {
-    name:"김밥",
+    id: 5,
+    name: "김밥",
     image: "https://i.ytimg.com/vi/eTucCw1w6Ak/maxresdefault.jpg",
-  }
-]
-
-
-function renderFood(dish){
-  return <Food name={dish.name} picture={dish.image}/>
-  //map은 언제나 array를 돌려줌
-}
+    rating: 3.5,
+  },
+];
 
 function App() {
   return (
     <div>
       <h1>Hello!!!!!</h1>
-       {foodILike.map(renderFood)}
+      {foodILike.map((dish) => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
+      ))}
     </div>
   );
 }
